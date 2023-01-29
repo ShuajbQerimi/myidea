c6548f13572ab2c8006e67179c0b4288c290a6ed
Shuajb Qerimi <shuajbqerimi@gmail.com>
2023-01-30 00:47:30 +0100

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
 $('#butoniservice1').click(function () {
     $('#img1service1').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#img2service1').css({
         'display': 'block',
+        "transition":"0.5s ease"
     });
     $('#butoniservice1').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#lineserviices1').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#textgraphhic1').css({
         'padding-bottom': '100px',
+        "transition":"0.5s ease"
     });
     $('#textservicess1').css({
         'display': 'block',
+        'cursor':'pointer',
+        "transition":"0.5s ease"
     });
     $('#kutiatext1').css({
         'background': '#292929',
@@ -23,27 +30,66 @@ $('#butoniservice1').click(function () {
         'height': '162px',
         'width': '1060px',
         'margin-bottom': '12px',
+        "transition":"0.5s ease"
+        
+    });
+});
+$('#textservicess1').click(function () {
+    $('#img1service1').css({
+        'display': 'flex',
+        "transition":"0.5s ease",
+    });
+    $('#img2service1').css({
+        'display': 'none',
+        "transition":"0.5s ease",
+    });
+    $('#butoniservice1').css({
+        'display': 'flex',
+        "transition":"0.5s ease",
+    });
+    $('#lineserviices1').css({
+        'display': 'flex',
+        "transition":"0.5s ease",
+    });
+    $('#textgraphhic1').css({
+        'padding-bottom': '0px',
+        "transition":"0.5s ease",
+    });
+    $('#textservicess1').css({
+        'display': 'none',
+        "transition":"0.5s ease",
+    });
+    $('#kutiatext1').css({
+        'background': 'none',
+        "transition":"0.5s ease",
         
     });
 });
 $('#butoniservice2').click(function () {
     $('#img1service2').css({
         'display': 'none',
+        "transition":"0.5s ease",
     });
     $('#img2service2').css({
         'display': 'block',
+        "transition":"0.5s ease",
     });
     $('#butoniservice2').css({
         'display': 'none',
+        "transition":"0.5s ease",
     });
     $('#lineserviices2').css({
         'display': 'none',
+        "transition":"0.5s ease",
     });
     $('#textgraphhic2').css({
         'padding-bottom': '100px',
+        "transition":"0.5s ease",
     });
     $('#textservicess2').css({
         'display': 'block',
+        'cursor':'pointer',
+        "transition":"0.5s ease",
     });
     $('#kutiatext2').css({
         'background': '#292929',
@@ -51,27 +97,67 @@ $('#butoniservice2').click(function () {
         'height': '162px',
         'width': '1060px',
         'margin-bottom': '12px',
+        "transition":"0.5s ease",
+        
+    });
+});
+$('#textservicess2').click(function () {
+    $('#img1service2').css({
+        'display': 'flex',
+        "transition":"0.5s ease"
+    });
+    $('#img2service2').css({
+        'display': 'none',
+        "transition":"0.5s ease"
+    });
+    $('#butoniservice2').css({
+        'display': 'flex',
+        "transition":"0.5s ease"
+    });
+    $('#lineserviices2').css({
+        'display': 'flex',
+        "transition":"0.5s ease"
+    });
+    $('#textgraphhic2').css({
+        'padding-bottom': '0px',
+        "transition":"0.5s ease"
+    });
+    $('#textservicess2').css({
+        'display': 'none',
+        'cursor':'pointer',
+        "transition":"0.5s ease"
+    });
+    $('#kutiatext2').css({
+        'background': 'none',
+        "transition":"0.5s ease"
         
     });
 });
 $('#butoniservice3').click(function () {
     $('#img1service3').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#img2service3').css({
         'display': 'block',
+        "transition":"0.5s ease"
     });
     $('#butoniservice3').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#lineserviices3').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#textgraphhic3').css({
         'padding-bottom': '100px',
+        "transition":"0.5s ease"
     });
     $('#textservicess3').css({
         'display': 'block',
+        'cursor':'pointer',
+        "transition":"0.5s ease"
     });
     $('#kutiatext3').css({
         'background': '#292929',
@@ -79,27 +165,67 @@ $('#butoniservice3').click(function () {
         'height': '162px',
         'width': '1060px',
         'margin-bottom': '12px',
+        "transition":"0.5s ease"
+        
+    });
+});
+$('#textservicess3').click(function () {
+    $('#img1service3').css({
+        'display': 'flex',
+        "transition":"0.5s ease",
+    });
+    $('#img2service3').css({
+        'display': 'none',
+        "transition":"0.5s ease",
+    });
+    $('#butoniservice3').css({
+        'display': 'flex',
+        "transition":"0.5s ease",
+    });
+    $('#lineserviices3').css({
+        'display': 'flex',
+        "transition":"0.5s ease",
+    });
+    $('#textgraphhic3').css({
+        'padding-bottom': '0px',
+        "transition":"0.5s ease",
+    });
+    $('#textservicess3').css({
+        'display': 'none',
+        'cursor':'pointer',
+        "transition":"0.5s ease",
+    });
+    $('#kutiatext3').css({
+        'background': 'none',
+        "transition":"0.5s ease",
         
     });
 });
 $('#butoniservice4').click(function () {
     $('#img1service4').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#img2service4').css({
         'display': 'block',
+        "transition":"0.5s ease"
     });
     $('#butoniservice4').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#lineserviices4').css({
         'display': 'none',
+        "transition":"0.5s ease"
     });
     $('#textgraphhic4').css({
         'padding-bottom': '100px',
+        "transition":"0.5s ease"
     });
     $('#textservicess4').css({
         'display': 'block',
+        'cursor':'pointer',
+        "transition":"0.5s ease"
     });
     $('#kutiatext4').css({
         'background': '#292929',
@@ -107,12 +233,45 @@ $('#butoniservice4').click(function () {
         'height': '162px',
         'width': '1060px',
         'margin-bottom': '120px',
+        "transition":"0.5s ease"
+        
+    });
+});
+$('#textservicess4').click(function () {
+    $('#img1service4').css({
+        'display': 'flex',
+        "transition":"0.5s ease"
+    });
+    $('#img2service4').css({
+        'display': 'none',
+        "transition":"0.5s ease"
+    });
+    $('#butoniservice4').css({
+        'display': 'flex',
+        "transition":"0.5s ease"
+    });
+    $('#lineserviices4').css({
+        'display': 'flex',
+        "transition":"0.5s ease"
+    });
+    $('#textgraphhic4').css({
+        'padding-bottom': '0px',
+        "transition":"0.5s ease"
+    });
+    $('#textservicess4').css({
+        'display': 'none',
+        'cursor':'pointer',
+        "transition":"0.5s ease"
+    });
+    $('#kutiatext4').css({
+        'background': 'none',
+        "transition":"0.5s ease"
         
     });
 });
 
-
 let active = false
+
 
 function toggle() {
     let toggle = document.querySelector('.toggle')
@@ -159,4 +318,155 @@ $(document).ready(function(){
         $('#spani4').hide(300);
     });
 });
+
+
+// Get the modal
+var myModal1 = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var myBtnmodal1 = document.getElementById("myBtnmodal1");
+
+// Get the <span> element that closes the modal
+var closemodal1 = document.getElementsByClassName("closemodal1")[0];
+
+// When the user clicks the button, open the modal 
+myBtnmodal1.onclick = function() {
+    myModal1.style.opacity = 1;
+    myModal1.style.display = "block";
+    myModal1.style.transition = "opacity 5s ease-in-out";
+    
+}
+
+// When the user clicks on <span> (x), close the modal
+closemodal1.onclick = function() {
+    myModal1.style.opacity = 0;
+    myModal1.style.display = "none";
+    myModal1.style.transition = "opacity 5s ease-in-out";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == myModal1) {
+    myModal1.style.display = "none";
+    myModal1.style.transition = "opacity 5s ease-in-out";
+  }
+}
+
+let project11 = document.querySelector('.project11image')
+let project12 = document.querySelector('.project12image')
+let project13 = document.querySelector('.project13image')
+let project14 = document.querySelector('.project14image')
+let project15 = document.querySelector('.project15image')
+
+let project11video = document.querySelector('.project11video')
+let project12video = document.querySelector('.project12video')
+let project13video = document.querySelector('.project13video')
+let project14video = document.querySelector('.project14video')
+let project15video = document.querySelector('.project15video')
+
+
+
+let changetovideos1 = document.querySelector('.changetovideo')
+let changetoimages1 = document.querySelector('.changetoimages')
+let imagespart = document.querySelector('.imagespart')
+let videopart = document.querySelector('.videopart')
+
+
+
+
+
+
+function button1image() {
+    project11.style.display = 'block';
+    project12.style.display = 'none';
+    project13.style.display = 'none';
+    project14.style.display = 'none';
+    project15.style.display = 'none';
+}
+
+
+function button1image() {
+        project11.style.display = 'block';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'none';   
+}
+function button2image() {
+        project11.style.display = 'none';
+        project12.style.display = 'block';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+}
+function button3image() {
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'block';
+        project14.style.display = 'none';
+        project15.style.display = 'none'; 
+}
+function button4image() {
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'block';
+        project15.style.display = 'none';
+}
+function button5image() {
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'block';
+}
+function button1video() {
+    project11video.style.display = 'block';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+}
+function button2video() {
+    project11video.style.display = 'none';
+    project12video.style.display = 'block';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+}
+function button3video() {
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'block';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+}
+function button4video() {
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'block';
+    project15video.style.display = 'none';
+}
+function button5video() {
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'block';
+}
+
+function changetovideo() {
+    imagespart.style.display = 'none';
+    videopart.style.display = 'flex';
+    changetovideos1.style.color = '#FFFF27';
+    changetoimages1.style.color= '#FFF';
+}
+function changetoimages() {
+    imagespart.style.display = 'flex';
+    videopart.style.display = 'none';
+    changetovideos1.style.color = '#FFF';
+    changetoimages1.style.color= '#FFFF27';
+}
+
 
