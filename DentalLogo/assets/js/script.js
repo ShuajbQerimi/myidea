4f431b9b881a452e28a3c7359b73f6b06c68ae3c
Shuajb Qerimi <shuajbqerimi@gmail.com>
2023-03-27 01:32:14 +0200

    Sd

---
+++ DentalLogo/assets/js/script.js
@@ -0,0 +1,167 @@
+let project11 = document.querySelector('.project11image')
+let project12 = document.querySelector('.project12image')
+let project13 = document.querySelector('.project13image')
+let project14 = document.querySelector('.project14image')
+let project15 = document.querySelector('.project15image')
+let project16 = document.querySelector('.project16image')
+let project17 = document.querySelector('.project17image')
+document.getElementById("changenr").innerHTML = "1";
+
+let project11video = document.querySelector('.project11video')
+let project12video = document.querySelector('.project12video')
+let project13video = document.querySelector('.project13video')
+let project14video = document.querySelector('.project14video')
+let project15video = document.querySelector('.project15video')
+
+
+
+let changetovideos1 = document.querySelector('.changetovideo')
+let changetoimages1 = document.querySelector('.changetoimages')
+let imagespart = document.querySelector('.imagespart')
+let videopart = document.querySelector('.videopart')
+
+
+
+
+
+
+function button1image() {
+    project11.style.display = 'block';
+    project12.style.display = 'none';
+    project13.style.display = 'none';
+    project14.style.display = 'none';
+    project15.style.display = 'none';
+    project16.style.display = 'none';
+    project17.style.display = 'none';
+    document.getElementById("changenr").innerHTML = "1";
+
+}
+
+
+function button1image() {
+        project11.style.display = 'block';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+        project16.style.display = 'none';
+        document.getElementById("changenr").innerHTML = "1";
+    
+}
+function button2image() {
+
+        project11.style.display = 'none';
+        project12.style.display = 'block';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+        project16.style.display = 'none';
+        document.getElementById("changenr").innerHTML = "2";
+    
+}
+function button3image() {
+ 
+
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'block';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+        project16.style.display = 'none';
+        document.getElementById("changenr").innerHTML = "3";
+    
+}
+function button4image() {
+
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'block';
+        project15.style.display = 'none';
+        project16.style.display = 'none';
+        document.getElementById("changenr").innerHTML = "4";
+    
+}
+function button5image() {
+
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'block';
+        project16.style.display = 'none';
+        document.getElementById("changenr").innerHTML = "5";
+}
+function button6image() {
+
+        project11.style.display = 'none';
+        project12.style.display = 'none';
+        project13.style.display = 'none';
+        project14.style.display = 'none';
+        project15.style.display = 'none';
+        project16.style.display = 'block';
+        document.getElementById("changenr").innerHTML = "6";
+}
+
+
+
+function button1video() {
+    project11video.style.display = 'block';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+
+}
+function button2video() {
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'block';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+
+}
+function button3video() {
+
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'block';
+    project14video.style.display = 'none';
+    project15video.style.display = 'none';
+
+}
+function button4video() {
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'block';
+    project15video.style.display = 'none';
+
+}
+function button5video() {
+
+    project11video.style.display = 'none';
+    project12video.style.display = 'none';
+    project13video.style.display = 'none';
+    project14video.style.display = 'none';
+    project15video.style.display = 'block';
+}
+
+
+function changetovideo() {
+    imagespart.style.display = 'none';
+    videopart.style.display = 'flex';
+    changetovideos1.style.color = '#FFFF27';
+    changetoimages1.style.color= '#FFF';
+}
+function changetoimages() {
+    imagespart.style.display = 'flex';
+    videopart.style.display = 'none';
+    changetovideos1.style.color = '#FFF';
+    changetoimages1.style.color= '#FFFF27';
+}
+
+
